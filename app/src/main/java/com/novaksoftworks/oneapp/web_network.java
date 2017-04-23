db9a1037778c110864f0be2d5132c2fd84785266
Hunter Line <contact@hunterline.info>
2017-04-23 12:39:26 -0400

    Trying to be able to view all 4 websites instead of just Softworks

--- app/src/main/java/com/novaksoftworks/oneapp/web_network.java
+++ app/src/main/java/com/novaksoftworks/oneapp/web_network.java
@@ -2,27 +2,22 @@ package com.novaksoftworks.oneapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class web_network extends Activity {
-    private class MyWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
     Button btnBack;
     WebView webview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.network);
 
         webview=(WebView)findViewById(R.id.netweb);
-        webview.setWebViewClient(new MyWebViewClient());
+        webview.setWebViewClient(new NetworkWebView());
         openURL();
     }
 
@@ -31,9 +26,18 @@ public class web_network extends Activity {
         webview.loadUrl("https://novaknetwork.com");
         webview.requestFocus();
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         this.finish();
+    }
+
+    private class NetworkWebView extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.getUrl();
+            return true;
+        }
     }
 }
